7626989613a943ff15a7c93854d1b245db6bf666
Sofyan Eko Sanjoyo <sofyanekosanjoyo@pu.go.id>
2022-11-18 13:52:50 +0700

    Sistem Informasi Pegawai ASN Sederhana

---
+++ admin/pegawai/del_pegawai.php
@@ -0,0 +1,28 @@
+<?php
+
+if(isset($_GET['kode'])){
+    $sql_cek = "select * from tb_pegawai where nip='".$_GET['kode']."'";
+    $query_cek = mysqli_query($koneksi, $sql_cek);
+    $data_cek = mysqli_fetch_array($query_cek,MYSQLI_BOTH);
+}
+?>
+
+<?php
+    $foto= $data_cek['foto'];
+    if (file_exists("foto/$foto")){
+        unlink("foto/$foto");
+    }
+
+    $sql_hapus = "DELETE FROM tb_pegawai WHERE nip='".$_GET['kode']."'";
+    $query_hapus = mysqli_query($koneksi, $sql_hapus);
+    if ($query_hapus) {
+        echo "<script>
+        Swal.fire({title: 'Hapus Data Berhasil',text: '',icon: 'success',confirmButtonText: 'OK'
+        }).then((result) => {if (result.value) {window.location = 'index.php?page=data-pegawai'
+        ;}})</script>";
+        }else{
+        echo "<script>
+        Swal.fire({title: 'Hapus Data Gagal',text: '',icon: 'error',confirmButtonText: 'OK'
+        }).then((result) => {if (result.value) {window.location = 'index.php?page=data-pegawai'
+        ;}})</script>";
+    }
---
+++ inc/koneksi.php
@@ -0,0 +1,5 @@
+<?php
+$koneksi = new mysqli ("localhost","root","","data_pegawai");
+?>
+
+<!-- end -->
